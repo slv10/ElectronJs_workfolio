d2309bf09ed2b537215f534e3955d48baf53d242
Mehul <placedownmehul@gmail.com>
2024-06-30 01:35:53 +0530

    Updated View and error messages/successful popup

--- login-page-renderer/assets/JS/main.js
+++ login-page-renderer/assets/JS/main.js
@@ -1,9 +1,56 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const signInAttempted = document.getElementById('sign_In');
+const Email = document.querySelector('#registered-email');
+const Pass = document.querySelector('#registered-pass');
 
 signUpButton.addEventListener('click', () =>
-container.classList.add('right-panel-active'));
+    container.classList.add('right-panel-active'));
 
 signInButton.addEventListener('click', () =>
-container.classList.remove('right-panel-active'));
+    container.classList.remove('right-panel-active'));
+
+signInAttempted.addEventListener('click', () => {
+    console.log('signin clicked');
+
+    if (Email.value === '' || Pass.value === '') {
+        alertError('Please enter Email and Password');
+        return;
+    }
+    ipcRenderer.send('sign-in-attempted', {
+        Email,
+        Pass
+    });
+});
+
+ipcRenderer.on('sign-in-successful', () => {
+    console.log("logged in");
+    alertSuccess('Successfully Logged in');
+});
+
+function alertSuccess(message) {
+    Toastify.toast({
+        text: message,
+        duration: 5000,
+        close: false,
+        style: {
+            background: 'green',
+            color: 'white',
+            textAlign: 'center'
+        },
+    });
+}
+
+function alertError(message) {
+    Toastify.toast({
+        text: message,
+        duration: 4000,
+        close: false,
+        style: {
+            background: 'red',
+            color: 'white',
+            textAlign: 'center',
+        },
+    });
+}
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
-const {app, BrowserWindow, ipcMain} = require('electron');
+const { app, BrowserWindow, ipcMain, ipcRenderer } = require('electron');
+const { error } = require('console');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
@@ -20,18 +21,28 @@ function createMainWindow() {
   });
 
   // Show devtools automatically if in development
-//   if (isDev) {
-//     mainWindow.webContents.openDevTools();
-//   }
+  //   if (isDev) {
+  //     mainWindow.webContents.openDevTools();
+  //   }
 
-    // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
-   mainWindow.loadFile(path.join(__dirname, './login-page-renderer/index.html'));
+  // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
+  mainWindow.loadFile(path.join(__dirname, './login-page-renderer/index.html'));
 }
 
 // When the app is ready, create the window
 app.on('ready', () => {
   createMainWindow();
-// There should be something for footer just like menu
+  // There should be something for footer just like menu
   // Remove variable from memory
   mainWindow.on('closed', () => (mainWindow = null));
+});
+
+ipcMain.on('sign-in-attempted', () => {
+  try {
+    mainWindow.loadFile(path.join(__dirname, './activity-renderer/index.html'));
+    ipcRenderer.send('sign-in-successful');
+  }
+  catch (err) {
+    console.log(err);
+  }
 });
